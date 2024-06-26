54dd8248992d0ce924e8bde909cad94be5174944
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-06-26 21:57:11 +0800

    fix ResetHotnessCounter

--- lsplant/src/main/jni/art/runtime/jit/jit_code_cache.hpp
+++ lsplant/src/main/jni/art/runtime/jit/jit_code_cache.hpp
@@ -26,6 +26,35 @@ class JitCodeCache {
                                    backup(thiz, self);
                                });
 
+    CREATE_MEM_FUNC_SYMBOL_ENTRY(void*, GetProfilingInfo, JitCodeCache *thiz,
+                                 ArtMethod *old_method, Thread *self) {
+        if (GetProfilingInfoSym) [[likely]] {
+            return GetProfilingInfoSym(thiz, old_method, self);
+        } else {
+            return nullptr;
+        }
+    }
+
+    CREATE_MEM_HOOK_STUB_ENTRY("_ZN3art3jit12JitCodeCache19ResetHotnessCounterEPNS_9ArtMethodEPNS_6ThreadE", void,
+                               ResetHotnessCounter, (JitCodeCache * thiz, ArtMethod* method, Thread *self), {
+                                   if (GetProfilingInfo(thiz, method, self) == nullptr) {
+                                       ArtMethod *original = nullptr;
+                                       for (auto [orig, bak]: hooked_methods_) {
+                                           if (bak.second == method) {
+                                               original = orig;
+                                               break;
+                                           }
+                                       }
+                                       if (original) {
+                                           LOGD("fix ResetHotnessCounter: backup %p -> original %p", method, original);
+                                           method = original;
+                                       } else {
+                                           LOGE("fix ResetHotnessCounter: no backup found for %p", method);
+                                       }
+                                   }
+                                   backup(thiz, method, self);
+                               });
+
 public:
     static bool Init(const HookHandler &handler) {
         auto sdk_int = GetAndroidApiLevel();
@@ -40,6 +69,13 @@ public:
         if (sdk_int >= __ANDROID_API_N__) [[likely]] {
             if (!HookSyms(handler, GarbageCollectCache)) [[unlikely]] {
                 return false;
+            }
+        }
+        if (!RETRIEVE_MEM_FUNC_SYMBOL(GetProfilingInfo, "_ZN3art3jit12JitCodeCache16GetProfilingInfoEPNS_9ArtMethodEPNS_6ThreadE")) {
+            LOGE("GetProfilingInfo not found!");
+        } else {
+            if (!HookSyms(handler, ResetHotnessCounter)) {
+                LOGE("failed to hook ResetHotnessCounter!");
             }
         }
         return true;
